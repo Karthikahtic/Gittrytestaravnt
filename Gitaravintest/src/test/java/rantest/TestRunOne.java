f11ba50ce5c00084bf5db7c9b1f227098ec24f4b
HTIC1905 <karthika@hticglobal.com>
2026-02-04 18:04:59 +0530

    Code Added

--- Gitaravintest/src/test/java/rantest/TestRunOne.java
+++ Gitaravintest/src/test/java/rantest/TestRunOne.java
@@ -1,4 +1,12 @@
 package rantest;
 
+
+import org.testng.annotations.Test;
+
 public class TestRunOne {
+ @Test
+    public void dimTest(){
+     System.out.println("HUT");
+ }
+
 }
---
+++ Gitaravintest/src/test/java/rantest/TukTestOne.java
@@ -0,0 +1,11 @@
+package rantest;
+
+import org.testng.annotations.Test;
+
+public class TukTestOne {
+
+    @Test
+    public void getTest(){
+        System.out.println("JUY");
+    }
+}
